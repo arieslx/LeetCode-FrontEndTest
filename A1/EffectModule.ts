1c9dc55d7f60209380a2f056c8a5c1cedea3ff81
arieslx <arieslxliu@gmail.com>
2019-08-23 21:52:34 +0800

    update：第一题解答

--- A1/EffectModule.ts
+++ A1/EffectModule.ts
@@ -1,63 +1,62 @@
 interface Action<T> {
     payload?: T;
     type: string;
-  }
-  
-  class EffectModule {
+}
+
+class EffectModule {
     count = 1;
     message = "hello!";
-  
+
     delay(input: Promise<number>) {
-      return input.then(i => ({
-        payload: `hello ${i}!`,
-        type: 'delay'
-      }))
+        return input.then(i => ({
+            payload: `hello ${i}!`,
+            type: 'delay'
+        }))
     }
-  
+
     setMessage(action: Action<Date>) {
-      return {
-        payload: action.payload!.getMilliseconds(),
-        type: "set-message"
-      };
+        return {
+            payload: action.payload!.getMilliseconds(),
+            type: "set-message"
+        };
     }
-  }
-  
-  type AsyncMethod<T, U> = (input: Promise<T>) => Promise<Action<U>>
-  type SyncMethod<T, U> = (action: Action<T>) => Action<U>
-  
-  type AllowedKeys<T, P> = {
-    [K in keyof T]: T[K] extends P ? K : never
-  }[keyof T]
-  
-  type Filter<T, P> = Pick<T, AllowedKeys<T, P>>
-  
-  type Method<T> = Filter<T, Function>
-  
-  type ConnectedModule<M> = {
-    [K in keyof Method<M>]:
-      Method<M>[K] extends AsyncMethod<infer T, infer U>
-      ? (input: T) => Action<U>
-      : Method<M>[K] extends SyncMethod<infer T, infer U>
-        ? (action: T) => Action<U>
-        : never
-  }
-  
-  type Connect = (module: EffectModule) => ConnectedModule<EffectModule>
-  
-  const connect: Connect = m => ({
+}
+
+// 1. 过滤非函数属性
+type FuncFilter<T> = {
+    [P in keyof T]: T[P] extends Function ? P : never;
+}[keyof T];
+
+type EffectModuleFunc = FuncFilter<EffectModule>;
+
+
+// 2. 方法映射
+type resolveEffectModuleFunc<T> =
+    T extends (input: Promise<infer U>) => Promise<Action<infer V>>
+    ? (input: U) => Action<V>
+    : T extends (action: Action<infer U>) => Action<infer V>
+    ? (action: U) => Action<V>
+    : never;
+
+// 3. 修改Connect类型
+type Connect = (module: EffectModule) => {
+    [T in EffectModuleFunc]: resolveEffectModuleFunc<EffectModule[T]>
+}
+
+const connect: Connect = m => ({
     delay: (input: number) => ({
-      type: "delay",
-      payload: `hello 2`
+        type: "delay",
+        payload: `hello 2`
     }),
     setMessage: (input: Date) => ({
-      type: "set-message",
-      payload: input.getMilliseconds()
+        type: "set-message",
+        payload: input.getMilliseconds()
     })
-  });
-  
-  type Connected = {
+});
+
+type Connected = {
     delay(input: number): Action<string>;
     setMessage(action: Date): Action<number>;
-  };
-  
-  export const connected: Connected = connect(new EffectModule());
+};
+
+export const connected: Connected = connect(new EffectModule());
