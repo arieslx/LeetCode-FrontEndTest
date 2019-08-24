6513a6eefa1846072abcdb0228be78eb5187f088
arieslx <arieslxliu@gmail.com>
2019-08-24 16:54:19 +0800

    update: webpack config

--- A2/webpack.config.js
+++ A2/webpack.config.js
@@ -12,6 +12,25 @@ module.exports = {
 
   optimization: {
     runtimeChunk: true,
-    splitChunks: {},
-  },
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/
+        },
+        common: {
+          test: /[\\/]src[\\/]common[\\/]/,
+          minSize: 0
+        },
+        module1: {
+          test: /[\\/]src[\\/]module-1[\\/]/,
+          enforce: true
+        },
+        module2: {
+          test: /[\\/]src[\\/]module-2[\\/]/,
+          enforce: true
+        }
+      }
+    },
+  }
 }
